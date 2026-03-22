52ba7b24d7b514bdb9f541e89a5d9ddabaf985d4
nkcfan <nkcfan@users.noreply.github.com>
2026-03-22 05:57:55 +0000

    Milestone 7b: Fix Fever setup - enable system-level api_enabled in data/config.php

--- Docker/entrypoint-wrapper.sh
+++ Docker/entrypoint-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # FreshRSS multi-category entrypoint wrapper
-# If FRESHRSS_API_PASSWORD is set, auto-configures the Fever API key for the default user.
+# If FRESHRSS_API_PASSWORD is set, auto-enables API and configures Fever key for the default user.
 
 setup_fever() {
     local user
@@ -11,27 +11,35 @@ setup_fever() {
         return 1
     fi
 
-    # Fix ownership so www-data can write config
+    # Fix ownership so www-data can write configs
     chown -R www-data:www-data "/var/www/FreshRSS/data/users/${user}" 2>/dev/null
+    chown www-data:www-data /var/www/FreshRSS/data/config.php 2>/dev/null
 
-    # Set API password
+    # Set API password for user
     php /var/www/FreshRSS/cli/update-user.php \
         --user "$user" --api-password "$FRESHRSS_API_PASSWORD" >/dev/null 2>&1
 
-    # Enable API and create Fever key file
+    # Enable API globally + per-user, create Fever key file
     php -r "
 define('FRESHRSS_PATH', '/var/www/FreshRSS');
 define('DATA_PATH', '/var/www/FreshRSS/data');
 require '/var/www/FreshRSS/constants.php';
 require LIB_PATH.'/lib_rss.php';
 FreshRSS_Context::initSystem();
-FreshRSS_Context::initUser('$user');
 
+// Enable API globally
+\$sysConf = FreshRSS_Context::systemConf();
+\$sysConf->api_enabled = true;
+\$sysConf->save();
+
+// Enable API for user
+FreshRSS_Context::initUser('$user');
 \$conf = FreshRSS_Context::userConf();
 \$conf->api_enabled = true;
 \$conf->save();
 
 // Re-init to pick up feverKey set by update-user.php
+FreshRSS_Context::initSystem();
 FreshRSS_Context::initUser('$user');
 \$salt = FreshRSS_Context::systemConf()->salt;
 \$feverKey = FreshRSS_Context::userConf()->feverKey;
